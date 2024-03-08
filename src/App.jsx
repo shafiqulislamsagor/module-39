88e8a046d2bc168448b190d8adcc5dc7821b90d4
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-03-08 22:30:14 +0600

    button handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function handleClick(){
+    alert('button clicked')
+  }
+  const handleClick2 = () =>{
+    alert('button 2 click')
+  }
+  const sum = (num) =>{
+    return alert(num + 10);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>Click Me</button> 
+      <button onClick={handleClick2}>Click Me2</button> 
+      <button onClick={() => {alert('hi suna')}}>Click Me3</button> 
+      <button onClick={() => sum(5)}>sumthing value</button>
+      
     </>
   )
 }
